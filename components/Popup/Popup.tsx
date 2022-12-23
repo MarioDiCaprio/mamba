9587f9b1ed409e355dc9463b1572126f3832e2a4
MarioDiCaprio <mariodicaprio10@gmail.com>
2022-12-23 14:59:24 +0100

    Created Popup component

---
+++ components/Popup/Popup.tsx
@@ -0,0 +1,32 @@
+import styles from "./Popup.module.sass";
+import React from "react";
+
+
+interface PopupProps {
+    open?: boolean;
+    onOutsideClick?: () => void;
+    children?: React.ReactNode;
+}
+
+
+const Popup: React.FC<PopupProps> = ({ children, onOutsideClick, open=false }) => {
+    const css = [styles.main];
+    if (open) {
+        css.push(styles.open);
+    }
+    const classNames = css.join(' ');
+
+    function handleOutsideClick(event: any) {
+        if (onOutsideClick !== undefined && event.target.className === classNames) onOutsideClick();
+    }
+
+    return (
+        <div className={classNames} onClick={handleOutsideClick} data-test="popup">
+            <div className={styles.content}>
+                { children }
+            </div>
+        </div>
+    );
+}
+
+export default Popup;
