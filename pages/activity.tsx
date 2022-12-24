96638b8c310f829ffd1f3cf921d73159322058b4
MarioDiCaprio <mariodicaprio10@gmail.com>
2022-12-24 17:47:28 +0100

    Added activity page

---
+++ pages/activity.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+import Base from "../components/Base/Base";
+
+
+const Activity: NextPage = () => {
+    return (
+        <Base />
+    );
+}
+
+
+export default Activity;
